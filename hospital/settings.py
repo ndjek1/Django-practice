cf7edb8401ef7aadde9048f8f54e4d9b2c2292b7
Ndjekornom Victoire <vndjekornom@gmail.com>
2025-03-19 20:26:25 +0300

    added database

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@cdny-^#^od^h5j0g1c!rt&6lh-yv2p_16vcioe0*ax^y$a9zv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
